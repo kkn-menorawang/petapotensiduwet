3e00bf7d98cb3d422223df21c5029cb7fa2e0cfa
kkn-menorawang <menorawang@gmail.com>
2026-08-02 11:01:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -51,3 +51,87 @@ const layerControl = L.control.layers(
         collapsed:false
     }
 ).addTo(map);
+
+
+/* ==========================================
+   PANE
+========================================== */
+
+map.createPane("batasDesa");
+map.getPane("batasDesa").style.zIndex = 300;
+
+map.createPane("batasPadukuhan");
+map.getPane("batasPadukuhan").style.zIndex = 301;
+
+map.createPane("sungai");
+map.getPane("sungai").style.zIndex = 302;
+
+map.createPane("jalan");
+map.getPane("jalan").style.zIndex = 303;
+
+map.createPane("fasilitas");
+map.getPane("fasilitas").style.zIndex = 304;
+
+map.createPane("potensi");
+map.getPane("potensi").style.zIndex = 305;
+
+map.createPane("landmark");
+map.getPane("landmark").style.zIndex = 306;
+
+
+/* ==========================================
+   LOAD GEOJSON
+========================================== */
+
+function loadGeoJSON(url, options, layerName){
+
+    fetch(url)
+
+    .then(response => response.json())
+
+    .then(data => {
+
+        const layer = L.geoJSON(data, options);
+
+        layer.addTo(map);
+
+        overlayMaps[layerName] = layer;
+
+        layerControl.addOverlay(layer, layerName);
+
+    })
+
+    .catch(error => {
+
+        console.error("Gagal memuat :", url);
+
+    });
+
+}
+
+
+loadGeoJSON(
+"data/jalan.geojson",
+{
+    pane:"jalan",
+
+    style:{
+        color:"#555",
+        weight:2
+    }
+
+},
+"Jalan"
+);
+
+
+
+fetch("data/batas_padukuhan.geojson")
+.then(r=>r.json())
+.then(data=>{
+
+    const batas = L.geoJSON(data);
+
+    map.fitBounds(batas.getBounds());
+
+});
